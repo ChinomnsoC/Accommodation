55902e2f8a91e468d0a0d6e5252cfecd3d88faa5
Chinomnso Chinedum <chinomnsoc@gmail.com>
2025-01-09 15:51:15 +0000

    removing python bits for backend

--- backend/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.pool import StaticPool
-
-DATABASE_URL = "postgresql://your_username:asdf@your_host:5432/your_database_name"
-
-engine = create_engine(DATABASE_URL, poolclass=StaticPool)  # Use StaticPool for Poetry
